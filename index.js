8ef1e28ec2efe367ec39a4652bd4f2ab8e08b35e
Olorunfemi <Femibam1@gmail.com>
2023-03-26 21:04:23 -0400

    whackmole

--- index.js
+++ index.js
@@ -5,18 +5,28 @@ const holes = document.querySelectorAll(".holes");
 const reset = document.querySelector(".reset");
 const board = document.querySelector(".hboard")
 
+// 1. Target holes 
+// 2. Target Image(goat)
+// 3. target score
+// 4. target hole board 
+// 5. add reset button at end 
 
 function display(){
- gameScore.innerHTML = gameCount;
+//  this will display the score in the browser
+ gameScore.innerHTML = gameCount; 
 
 }
 
 function randomHole(){
+    // function establishing random numbers for every hole on the board.
    const game = Math.floor(Math.random()* holes.length);
    return holes[game];
 
 }
 goat.addEventListener("click", function(){
+    // every time goat is clicked on the score will increase,
+    // it will display on browser, goat appears on different hole, and 
+    // background color changes to teal
     alert("NAILED IT!!!") 
     gameCount++ 
     display()
@@ -42,3 +52,6 @@ reset.addEventListener("click", function(){
 
 
 reset.addEventListener("click", resetGame);
+
+// reset button resets score, goat reappears within a random hole and 
+// an alert will pop up
