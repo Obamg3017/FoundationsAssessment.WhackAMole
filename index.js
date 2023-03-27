dad8cda1ae89f8eab8fd9d605f63a7911c2d51ac
Olorunfemi <Femibam1@gmail.com>
2023-03-26 20:20:47 -0400

    whackmole

--- index.js
+++ index.js
@@ -1,31 +1,47 @@
-const gamescore = document.querySelector("#gamescore")
-const goat = document.querySelector(".goat")
-const holes = document.querySelector(".holes")
-
 let gameCount = 0
+const gameScore = document.querySelector(".gamescore");
+const goat = document.querySelector(".goat");
+const holes = document.querySelectorAll(".holes");
+const reset = document.querySelector(".reset");
+const board = document.querySelector(".hboard")
 
-
-
-
-display()
 
 function display(){
-   gamescore.innerHTML = gameCount;
+ gameScore.innerHTML = gameCount;
+
 }
 
 function randomHole(){
-    const hole = Math.floor(Math.random()* holes.length);
-    return holes[hole];
-}
+   const game = Math.floor(Math.random()* holes.length);
+   return holes[game];
 
+}
 goat.addEventListener("click", function(){
-    alert("NAILED IT")
-    gameCount++
+    alert("NAILED IT!!!") 
+    gameCount++ 
     display()
     randomHole().appendChild(goat);
-})
+    board.style.backgroundColor = "Teal"
+});
 
 window.addEventListener("load", function(){
     randomHole().appendChild(goat);
-})
+});
 
+function resetGame(){
+    gameCount = 0;
+    display();
+    randomHole().appendChild(goat);
+    goat.removeChild(goat);
+    
+    
+}
+
+
+
+reset.addEventListener("click", function(){
+    alert("The Game has been reset...Try Whacking the Mole...I'm mean GOAT Again!!!")
+    board.style.backgroundColor = "whitesmoke"
+});
+
+reset.addEventListener("click", resetGame);
